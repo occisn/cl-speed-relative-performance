866b8e5c881fded2c473d7416cbc4bfcffebe49e
Nicolas O <noccis@I7413>
2025-04-06 16:15:59 +0200

    refactor: reorganize files to test byte and native compilations for Emacs Lisp

--- leibniz-elisp-A-interpreted.el
+++ leibniz-elisp-A-interpreted.el
@@ -1,4 +1,4 @@
-(defun leibniz-pi-1 (n)
+(defun leibniz-pi-A-1 (n)
   "Calculate an approximation of π using Leibniz formula with N terms."
   (let ((tmp 0.0)
         (sign 1.0))
@@ -6,7 +6,7 @@
       (setq tmp (+ tmp (/ sign (float (+ (* 2 i) 1)))))
       (setq sign (- sign)))))
 
-(defun leibniz-pi-2 (n)
+(defun leibniz-pi-A-2 (n)
   "Calculate an approximation of π using Leibniz formula with N terms."
   (let ((tmp 0.0)
         (sign t))
@@ -16,10 +16,10 @@
                  (- tmp (/ 1 (float (+ (* 2 i) 1))))))
       (setq sign (not sign)))))
 
-;; (leibniz-pi-1 10000000) ; 7 zeros
+;; (leibniz-pi-A-1 10000000) ; 7 zeros
 ;;    --> 3.1415925535897915 in several seconds
 
-;; (leibniz-pi-2 10000000) ; 7 zeros
+;; (leibniz-pi-A-2 10000000) ; 7 zeros
 ;;    --> 3.1415925535897915 in several seconds
 
 ;;; end
---
+++ leibniz-elisp-B-byte-compilation.el
@@ -0,0 +1,26 @@
+(defun leibniz-pi-B-1 (n)
+  "Calculate an approximation of π using Leibniz formula with N terms."
+  (let ((tmp 0.0)
+        (sign 1.0))
+    (dotimes (i n (* 4 tmp))
+      (setq tmp (+ tmp (/ sign (float (+ (* 2 i) 1)))))
+      (setq sign (- sign)))))
+
+(defun leibniz-pi-B-2 (n)
+  "Calculate an approximation of π using Leibniz formula with N terms."
+  (let ((tmp 0.0)
+        (sign t))
+    (dotimes (i n (* 4 tmp))
+      (setq tmp (if sign
+                   (+ tmp (/ 1 (float (+ (* 2 i) 1))))
+                 (- tmp (/ 1 (float (+ (* 2 i) 1))))))
+      (setq sign (not sign)))))
+
+;; (leibniz-pi-B-1 10000000) ; 7 zeros
+;;    --> 3.1415925535897915 in several seconds
+
+;; (leibniz-pi-B-2 10000000) ; 7 zeros
+;;    --> 3.1415925535897915 in several seconds
+
+
+;;; end
