117e39e787cb096d8e5c9d138e5d0a8cde2d330a
Nicolas O <noccis@I7413>
2025-04-19 21:07:48 +0200

    feat: add more clarity on Common Lisp code

--- leibniz-cl.lisp
+++ leibniz-cl.lisp
@@ -1,8 +1,9 @@
 
-(defparameter *n7* 10000000)   ; 7 zeros
-(defparameter *n8* 100000000)  ; 8 zeros
-(defparameter *n9* 1000000000) ; 9 zeros
-(declaim (type fixnum *n7* *n8* *n9*))
+(defparameter *n7* 10000000)     ;  7 zeros
+(defparameter *n8* 100000000)    ;  8 zeros
+(defparameter *n9* 1000000000)   ;  9 zeros
+(defparameter *n10* 10000000000) ; 10 zeros
+(declaim (type fixnum *n7* *n8* *n9* *n10*))
 
 ;;; change of flag:
 (defun leibniz-pi-1 ()
@@ -19,59 +20,140 @@
 ;;; change of sign:
 (defun leibniz-2 ()
   "Calculate an approximation of π using Leibniz formula."
-  (let ((tmp 0.0d0)
+  (let ((start-time (get-internal-real-time))
+        (tmp 0.0d0)
         (sign 1.0d0))
-    (dotimes (i *n8*)
+    (dotimes (i *n9*)
       (setq tmp (+ tmp (/ sign (+ (* 2 i) 1))))
       (setq sign (- sign)))
-    (* 4 tmp)))
+    (setq tmp (* 4 tmp))
+    (let* ((end-time (get-internal-real-time))
+           (duration (/ (- end-time start-time) (float internal-time-units-per-second))))
+      (format t "Result: ~,20f in ~f seconds~%" tmp duration)))
 
 ;;; change of sign + type, with no speed 3:
-(defun leibniz-3 ()
-  "Calculate an approximation of π using Leibniz formula."
-  (let ((tmp 0.0d0)
-        (sign 1))
-    (declare (type double-float tmp)
-             (type fixnum sign))
-    (dotimes (i *n8*)
-      (setq tmp (+ tmp (/ sign (+ (* 2 i) 1))))
-      (setq sign (- sign)))
-    (* 4 tmp)))
+  (defun leibniz-3 ()
+    "Calculate an approximation of π using Leibniz formula."
+    (declare (optimize (speed 0) (safety 3) (debug 0)))
+    (let ((tmp 0.0d0)
+          (sign 1))
+      (declare (type double-float tmp)
+               (type fixnum sign))
+      (dotimes (i *n8*)
+        (setq tmp (+ tmp (/ sign (+ (* 2 i) 1))))
+        (setq sign (- sign)))
+      (* 4 tmp))))
 
 ;;; change of sign + type and speed 3:
 (defun leibniz-4 ()
   "Calculate an approximation of π using Leibniz formula."
   (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (let ((tmp 0.0d0)
+  (let ((start-time (get-internal-real-time))
+        (tmp 0.0d0)
         (sign 1)
         (i 0))
     (declare (type double-float tmp)
              (type fixnum sign i))
-    (loop while (<= i *n8*)
+    (loop while (<= i *n10*)
           do (progn
                (setq tmp (+ tmp (/ sign (+ (* 2.0d0 i) 1.0d0)))
                      sign (- sign)
                      i (+ i 1))))
     (setq tmp (* 4.0d0 tmp))
-    (locally
-        (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
-      (format t "Result: ~,20f~%" tmp)))
+    (let* ((end-time (get-internal-real-time))
+           (duration (/ (- end-time start-time) (float internal-time-units-per-second))))
+      (locally
+          (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+        (format t "Result: ~,20f in ~f seconds~%" tmp duration))))
   nil)
 
+;;; 4-loop unrolling:
+(defun leibniz-5 ()
+  "Calculate an approximation of π using Leibniz formula."
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (let ((start-time (get-internal-real-time))
+        (nmax (- *n10* 3))
+        (tmp 0.0d0)
+        (i 0))
+    (declare (type double-float tmp))
+    (loop while (<= i nmax)
+          do (progn
+               (setq tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 i) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 1 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 2 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 3 i)) 1.0d0)))
+                     i (+ i 4))))
+    (setq tmp (* 4.0d0 tmp))
+    (let* ((end-time (get-internal-real-time))
+           (duration (/ (- end-time start-time) (float internal-time-units-per-second))))
+      (locally
+          (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+        (format t "Result: ~,20f in ~f seconds~%" tmp duration))))
+  nil)
 
-;; (leibniz-1) ; with 8 zeros
-;;    --> 3.141592643589326d0 in several seconds
+;;; 8-loop unrolling:
+(defun leibniz-6 ()
+  "Calculate an approximation of π using Leibniz formula."
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (let ((start-time (get-internal-real-time))
+        (nmax (- *n10* 7))
+        (tmp 0.0d0)
+        (i 0))
+    (declare (type double-float tmp))
+    (loop while (<= i nmax)
+          do (progn
+               (setq tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 i) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 1 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 2 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 3 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 4 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 5 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 6 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 7 i)) 1.0d0)))
+                     i (+ i 8))))
+    (setq tmp (* 4.0d0 tmp))
+    (let* ((end-time (get-internal-real-time))
+           (duration (/ (- end-time start-time) (float internal-time-units-per-second))))
+      (locally
+          (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+        (format t "Result: ~,20f in ~f seconds~%" tmp duration))))
+  nil)
 
-;; (leibniz-2) ; with 8 zeros
-;;    --> 3.141592643589326d0 in 3 seconds (unplugged)
+;;; 16-loop unrolling:
+(defun leibniz-7 ()
+  "Calculate an approximation of π using Leibniz formula."
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (let ((start-time (get-internal-real-time))
+        (nmax (- *n10* 15))
+        (tmp 0.0d0)
+        (i 0))
+    (declare (type double-float tmp))
+    (loop while (<= i nmax)
+          do (progn
+               (setq tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 i) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 1 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 2 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 3 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 4 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 5 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 6 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 7 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 8 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 9 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 10 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 11 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 12 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 13 i)) 1.0d0)))
+                     tmp (+ tmp (/ 1.0d0 (+ (* 2.0d0 (+ 14 i)) 1.0d0)))
+                     tmp (- tmp (/ 1.0d0 (+ (* 2.0d0 (+ 15 i)) 1.0d0)))
+                     i (+ i 16))))
+    (setq tmp (* 4.0d0 tmp))
+    (let* ((end-time (get-internal-real-time))
+           (duration (/ (- end-time start-time) (float internal-time-units-per-second))))
+      (locally
+          (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+        (format t "Result: ~,20f in ~f seconds~%" tmp duration))))
+  nil)
 
-;; (leibniz-3) ; with 8 zeros
-;;    --> 3.141592643589326d0 in 23 seconds (unplugged)
-;; type declaration slow down things if no (speed 3)
-
-;; (leibniz-4) is very quick
-;; 8 zeros --> 0.2 s
-;; 9 zeros --> 2 s
-;; (unplugged)
-
+ 
 ;;; end
